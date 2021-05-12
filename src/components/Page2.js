7dee1ca777b3897f631fd9f4eb851ded9ec93b2f
iphalgun <phalgun99@gmail.com>
2021-05-12 23:44:57 +0530

    - Page 2 added for portfolio

--- src/components/Page2.js
+++ src/components/Page2.js
@@ -1,7 +1,28 @@
+import img from '../assets/images/IMG_4016.JPG';
+
 function Page2(){
     return(
         <div>
-            Hi
+            <div class="content">
+                <div class="d-flex">
+                    <div class="col-md-6 col-lg-6 d-flex">
+                        <div class="img-about img d-flex align-items-stretch">
+                            <div class="img d-flex align-self-stretch align-items-center" style={{backgroundImage: 'url('+img+')'}}>
+                                {/* <img src={{img}}></img> */}
+                            </div>
+                        </div>
+                    {/* Hi */}
+                    </div>
+                    <div class="col-md-6 col-lg-6 pl-md-5 py-5">
+                        <div class="row justify-content-start pb-3">
+
+                        </div>
+                        <div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
